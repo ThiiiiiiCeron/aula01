4d9975b9c131a6fb7c165443747d48a1b0743934
Thiago <thiago.jc@aluno.ifsc.edu.br>
2022-02-24 17:02:35 -0300

    Projeto feito / Ler e imprimir o nome do unuário na classe scanner

---
+++ src/githuber/Githuber.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githuber;
+import java.util.Scanner;
+/**
+ *
+ * @author IFSC
+ */
+public class Githuber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leia= new Scanner (System.in);
+        String arros;
+        System.out.println("Digite seu nominho");
+        arros=leia.nextLine();
+        System.out.println(arros+"esse é o seu nominho muito lindo e maravilhoso!!!!!");
+    }
+    
+}
